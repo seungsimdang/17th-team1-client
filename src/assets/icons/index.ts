d6c5906c60cd8006c1ddea83232becca915dc363
yuminnnnnni <ka09023@ajou.ac.kr>
2025-09-24 00:25:00 +0900

    chore: svg 아이콘, index.ts 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CloseIcon } from "./close.svg";
+export { default as PlusIcon } from "./plus.svg";
+export { default as ReturnIcon } from "./return.svg";
+export { default as SearchIcon } from "./search.svg";
