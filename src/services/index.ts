236dfad5b2be6be732405641583bb0691b407ede
jiohjung98 <ckaquz98@naver.com>
2025-09-25 13:08:23 +0900

    refactor: 불필요한 API 서비스 제거

--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-// 모든 API 서비스를 한 곳에서 export
-export * from "./cityService";
-export * from "./authService";
-export * from "./globeService";
-
-// API 클라이언트도 함께 export
-export * from "../lib/apiClient";
